850278ef463d1877ad4ef91a49a6e98c2d4ca34d
Mohamad Ashraf <mohamad.shiro80026@gmail.com>
2024-05-07 23:50:50 +0300

    If ffmpeg path is not set, use default path.

--- src/util/ffmpeg.js
+++ src/util/ffmpeg.js
@@ -1,20 +1,41 @@
-import {spawn} from 'node:child_process'
-import ffmpegPath from 'ffmpeg-static'
+import { spawn } from "node:child_process";
+import { exec } from "node:child_process";
 
+let ffmpegPath = "";
+if (!process.env.FFMPEG_BIN) {
+  exec("which ffmpeg", (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.error(`Error: ${stderr}`);
+      return;
+    }
+    // The path to ffmpeg will be in the stdout
+    ffmpegPath = stdout.trim();
+  });
+} else {
+  ffmpegPath = process.env.FFMPEG_BIN;
+}
 
-export default (args, {silent = false, pipe, handleProgress = () => {}} = {}) => new Promise((resolve, reject) => {
-    const child = spawn(ffmpegPath, args)
-    let err = ''
+export default (
+  args,
+  { silent = false, pipe, handleProgress = () => {} } = {}
+) =>
+  new Promise((resolve, reject) => {
+    const child = spawn(ffmpegPath, args);
+    let err = "";
 
-    child.stdout.on('data', (data) => silent || console.log(data.toString()))
+    child.stdout.on("data", (data) => silent || console.log(data.toString()));
     // Listen for stderr output from FFmpeg process
-    child.stderr.on('data', (data) => {
-        handleProgress(data.toString());
+    child.stderr.on("data", (data) => {
+      handleProgress(data.toString());
     });
 
-    if (pipe) child.stderr.pipe(pipe)
+    if (pipe) child.stderr.pipe(pipe);
 
-    child.on('error', reject)
-    child.on('exit', (code) => code ? reject(err) : resolve())
-})
+    child.on("error", reject);
+    child.on("exit", (code) => (code ? reject(err) : resolve()));
+  });
 // spinner.text = `[${progressPercentage}%] Downloading ${colors.red('skate_phantom_flex_4k')} | Size: ${formatBytes(size)}`
